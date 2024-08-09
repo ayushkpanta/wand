e7f52ad01b5d5bfb36fbe89936ab6f49fce83615
Ayush Panta <ayushkpanta@gmail.com>
2024-08-08 20:04:00 -0700

    Learning sockets

---
+++ client.cpp
@@ -0,0 +1,52 @@
+// C++ program to illustrate the client application in the 
+// socket programming 
+#include <cstring> 
+#include <iostream> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <unistd.h> 
+
+// create, initialize, and connect a socket to an address
+// this is almost identical to the initialization of the server socket
+int initClientSocket(int port = 8080) {
+
+    // create socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specify local address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET; // IPv4 family
+    serverAddress.sin_port = htons(port); // port in network byte order
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // bind to all interfaces
+
+    // connect socket to address
+    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    return clientSocket;
+}
+
+// whatever activity the client may do
+// keeping it simple for now
+void clientActivity(int clientSocket) {
+    
+    // requests user input from console, sends to socket
+    std::string userInput;
+    std::cout << "Enter a message to send to the server:\n";
+    std::getline(std::cin, userInput);
+	const char* sendableMessage = userInput.c_str(); 
+	send(clientSocket, sendableMessage, strlen(sendableMessage), 0); 
+
+}
+
+int main() { 
+    // init socket in main
+    int clientSocket {initClientSocket()};
+
+	// activity and sending to server
+    clientActivity(clientSocket);
+
+	// closing socket 
+	close(clientSocket); 
+
+	return 0; 
+}
